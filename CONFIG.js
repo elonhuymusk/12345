b42f8f20b60ca574807382fb5d86bb4a3d2253f9
elonhuymusk <97786787+elonhuymusk@users.noreply.github.com>
2022-01-26 22:43:11 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu em nhé chị Quỳnh Trang ",
+  titleHeader: "Tết này đi chơi với tớ nhé <3 ",
   descriptionHeader:
-    "Nếu chị thoát ra và không trả lời thì có nghĩa là chị đã ĐỒNG Ý rồi đấy ^^",
+    "Thoát ra mà không trả lời là đồng ý đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn chị vì đồng ý",
-  descriptionModar:
-      'Em biết là chị Trang sẽ đồng ý làm người yêu emmà ^^ </br> Cám ơn chị đã đồng ý để cho em được che chở và bảo vệ chị.Đối với em, chị là một người tuyệt vời :)) </br> "Thích chị nhiều đến mấy, chị sẽ hiểu thôi. </br>  Em thích chị nhiều đến mấy, chị sẽ hiểu thôi "',
+  titleModar: "Đi chơi với tớ có phải vui không",
+  descriptionModar::
+      'Đi chơi Tết với tớ có phải vui hơn không ^^ </br> 2 người bao giờ chả vui hơn 1 người :))',
 };
